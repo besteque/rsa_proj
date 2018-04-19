34858738329d31a7e712200301d0aaab0ec692db
xuyang <johnir@126.com>
2018-04-19 14:31:44 +0800

    change e to bignum, instead of 65537

--- rsa_test.c
+++ rsa_test.c
@@ -6,7 +6,7 @@
 #include <gmp.h>  
   
 #define KEY_LENGTH 2048  //公钥的长度
-#define BASE 62    //输入输出的数字进制,62=sum(0-9,A-Z,a-z)，但未包含符号
+#define BASE 16    //输入输出的数字进制,62=sum(0-9,A-Z,a-z)，但未包含符号
 
 /*  
     note:apt-get install libgmp-dev (直接安装，或参照如下源码编译)
@@ -28,7 +28,7 @@ typedef struct tag_key_pair
 {  
     char n[KEY_LENGTH + 10]; 
     char d[KEY_LENGTH + 10];  
-    int e;  
+    char e[KEY_LENGTH + 10];  
 }key_pair;  
 
 #define PRINT_HEX(d, l)\
@@ -81,23 +81,51 @@ mpz_t * gen_primes()
 //生成密钥对
 key_pair * gen_key_pair()  
 {  
+    gmp_randstate_t grt;                  
+    gmp_randinit_default(grt);    
+    gmp_randseed_ui(grt, time(NULL));   
+    
     mpz_t * primes = gen_primes();  
   
-    mpz_t key_n, key_e, key_f;  
+    mpz_t key_n, key_e, key_fn, gcd;  
     mpz_init(key_n);  
-    mpz_init(key_f);  
+    mpz_init(key_fn);  
     mpz_init_set_ui(key_e, 65537);  //设置e为65537  
   
     mpz_mul(key_n, primes[0], primes[1]);       //计算n=p*q  
     mpz_sub_ui(primes[0], primes[0], 1);        //p=p-1  
     mpz_sub_ui(primes[1], primes[1], 1);        //q=q-1  
-    mpz_mul(key_f, primes[0], primes[1]);       //计算欧拉函数φ(n)=(p-1)*(q-1)  
-  
+    mpz_mul(key_fn, primes[0], primes[1]);       //计算欧拉函数φ(n)=(p-1)*(q-1)  
+
+    int loop = 0;
+    
+    do
+    {  
+        loop++;
+        //e = randprime(ebits);
+        mpz_urandomm(key_e, grt, key_fn);
+        mpz_gcd(gcd,key_e,key_fn);  
+    }while(mpz_get_ui(gcd)!=1);
+
+    gmp_printf ("loop:%d, key_e:%Zd\n", loop,key_e);
+    char buf_e[KEY_LENGTH + 10] = {0};  
+    char buf_fn[KEY_LENGTH + 10] = {0};  
+    mpz_get_str(buf_e, 16, key_e);
+    mpz_get_str(buf_fn, 16, key_fn);
+    gmp_printf ("key_fn:%Zd\n", key_fn);
+    printf("buf_e:%s\n", buf_e);
+    printf("buf_fn:%s\n", buf_fn);
+
+    //printf("key_fn%%key_e = %ld\n", mpz_get_ui(key_fn) % mpz_get_ui(key_e));
+    
+    //mpz_gcdext(p,d,q,key_e,key_fn); 
+
+    
     mpz_t key_d;      
     mpz_init(key_d);  
 
     // mpz_invert(rop, op1, op2)  ，op1*rop ≡ 1 (mod op2)暗示 (op1*rop - 1) mod op2 = 0
-    mpz_invert(key_d, key_e, key_f);   //计算数论倒数   
+    mpz_invert(key_d, key_e, key_fn);   //计算数论倒数   
   
     key_pair *result = malloc(sizeof(key_pair)); 
     bzero(result, sizeof(key_pair));
@@ -109,14 +137,16 @@ key_pair * gen_key_pair()
     memcpy(result->n, buf_n, KEY_LENGTH + 10);  
     mpz_get_str(buf_d, BASE, key_d);  
     memcpy(result->d, buf_d, KEY_LENGTH + 10);  
-    result->e = 65537;  
+    //result->e = 65537;  
+    mpz_get_str(buf_e, BASE, key_e);  
+    memcpy(result->e, buf_e, KEY_LENGTH + 10);  
   
     mpz_clear(primes[0]);   //释放内存  
     mpz_clear(primes[1]);  
     mpz_clear(key_n);  
     mpz_clear(key_d);  
     mpz_clear(key_e);  
-    mpz_clear(key_f);  
+    mpz_clear(key_fn);  
     
     free((char*)primes);  
   
@@ -124,34 +154,57 @@ key_pair * gen_key_pair()
 } 
 
 #if 0 //cpp
-void setKey(mpz_class &n, mpz_class &e, mpz_class &d,const int nbits,int ebits=16)  
-{  
-    if(nbits/2<=ebits) {  
+//void setKey(mpz_class &n, mpz_class &e, mpz_class &d,const int nbits,int ebits=16)  
+void setKey(const int nbits, int ebits)  
+{
+    mpz_t n, e, d, p, q, fn, gcd;
+
+    if(nbits/2<=ebits) 
+    {  
         ebits = nbits/2;  
     }  
-    mpz_class p = randprime(nbits/2);  
-    mpz_class q = randprime(nbits/2);  
+    p = randprime(nbits/2);  
+    q = randprime(nbits/2);  
     n = q*p;  
-    mpz_class fn = (p-1)*(q-1);  
-    mpz_class gcd;  
-    do{  
+    fn = (p-1)*(q-1);
+    
+    do
+    {  
         e = randprime(ebits);  
-        mpz_gcd(gcd.get_mpz_t(),e.get_mpz_t(),fn.get_mpz_t());  
-    }while(gcd!=1);  
-    mpz_gcdext(p.get_mpz_t(),d.get_mpz_t(),q.get_mpz_t(),e.get_mpz_t(),fn.get_mpz_t());  
+        mpz_gcd(gcd,e,fn);  
+    }while(gcd!=1);
+    
+    mpz_gcdext(p,d,q,e,fn); 
+
+
+    char buf_p[1024] = {0};
+    char buf_d[1024] = {0};
+    char buf_e[1024] = {0};
+    
+    mpz_get_str(buf_p, 16, p);
+    mpz_get_str(buf_d, 16, d);
+    mpz_get_str(buf_e, 16, e);
+
+    printf("buf_p:%s\n", buf_p);
+    printf("buf_d:%s\n", buf_d);
+    printf("buf_e:%s\n", buf_e);
+
+    return;
 }  
 
 #endif
 
 //加密函数  
-char * encrypt(const char * plain_text, const char * key_n, int key_e)    
+char * encrypt(const char * plain_text, const char * key_n, const char *key_e)    
 {  
-    mpz_t M, C, n;  
+    mpz_t M, C, n, e;  
     mpz_init_set_str(M, plain_text, BASE);   
     mpz_init_set_str(n, key_n, BASE);  
+    mpz_init_set_str(e, key_e, BASE);  
     mpz_init_set_ui(C, 0);  
   
-    mpz_powm_ui(C, M, key_e, n);    //使用GMP中模幂计算函数
+    //mpz_powm_ui(C, M, key_e, n);    //使用GMP中模幂计算函数
+    mpz_powm(C, M, e, n);    //使用GMP中模幂计算函数
   
     char *result = calloc(1, KEY_LENGTH + 10);  
     mpz_get_str(result, BASE, C);  
@@ -181,14 +234,19 @@ char * decrypt(const char * cipher_text, const char * key_n, const char * key_d)
 int main()  
 { 
     int i,j = 0, len;
-    key_pair * p = gen_key_pair();  
+    key_pair * p = gen_key_pair();
+
+    //setKey(1024, 17);
+    //printf("------------------over----------------\n");
+
+    
   
     printf("n = %s\n", p->n); 
     printf("d = %s\n", p->d);
-    printf("e = %d\n", p->e);
+    printf("e = %s\n", p->e);
 
     printf("\n----------------------------------->\n");
-    printf("public key(n,e):%s,%x\n", p->n, p->e);
+    printf("public key(n,e):%s,%s\n", p->n, p->e);
     printf("private key(n,d):%s,%s\n", p->n, p->d);
     printf("<-----------------------------------\n\n");
   
@@ -197,8 +255,8 @@ int main()
     char tmp[KEY_LENGTH + 10];  
     char swap[3] = {0};
     printf("请输入要加密的数字，二进制长度不超过%d\n",KEY_LENGTH); 
-    scanf("%s", buf);
-    //strcpy(buf, "./lk0cU-9C083GBFJB/KN V 8989WOH2KLN2W   P ");
+    //scanf("%s", buf);
+    strcpy(buf, "./lk0cU-9C083GBFJB/KN V 8989WOH2KLN2W   P ");
 
     len = strlen(buf);
     printf("input msg len:%d\n",len); 
@@ -213,7 +271,7 @@ int main()
     //end asc值转字符串
 
 
-    // 字符串转会asc值
+    // 字符串转回asc值
     j = 0;
     for (i = 0; i < len; i++)
     {
@@ -227,7 +285,7 @@ int main()
     }
     printf("true data as follow:\n"); 
     PRINT_HEX(tmp, len);
-    //end 字符串转会asc值
+    //end 字符串转回asc值
 
 
     
